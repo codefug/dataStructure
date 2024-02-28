27087ab762da035d499c9c66d3ca43e14088a024
codefug <robot9917@naver.com>
2024-02-29 00:51:42 +0900

    change LinkedList into Doubly circular linked list

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,40 +1,60 @@
 class LinkedList {
   length = 0;
   head = null;
-  //Create
+  tail = null;
   add(value) {
     this.length++;
-
     if (this.head) {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = new Node(value);
+      let node = new Node(value);
+      node.prev = this.tail;
+      node.next = this.head;
+      this.tail.next = node;
+      this.head.prev = node;
     } else {
-      this.head = new Node(value);
+      let node =new Node(value);
+      this.head = node;
+      this.tail = node;
+      this.head.prev = this.tail;
+      this.head.next = this.tail;
+      this.tail.prev=this.head;
+      this.tail.next = this.head;
     }
-
     return this.length;
   }
-  //Read
-  search(index) {
+  #prevCurrent(index) {
+    // private method
     let count = 0;
+    let prev;
     let current = this.head;
-    while (count <index){
+    while (count < index) {
+      prev = current;
       current = current?.next;
       count++;
     }
-    return current?.value;
+    return [prev, current];
+  }
+  //search
+  search(index) {
+    return this.#prevCurrent(index)[1]?.value;
   }
   //Delete
-  remove(value) {
-
+  remove(index) {
+    const [prev, current] = this.#prevCurrent(index);
+    if (prev !== undefined && current !== null) {
+      prev.next = current.next;
+      this.length--;
+      return this.length;
+    } else if (current !== null) {
+      this.head = current.next;
+      this.length--;
+      return this.length;
+    }
   }
 }
 
 class Node {
   next = null;
+  prev = null;
   constructor(value) {
     this.value = value;
   }
@@ -46,5 +66,5 @@ console.log(ll.add(1));
 console.log(ll.add(2));
 console.log(ll.add(3));
 console.log(ll.search(2));
-ll.search(1);
-ll.remove(1);
+console.log(ll.remove(1));
+console.log(ll.search(2));
